c0c803906161ca0b0dfc82903f837591b54ae65c
Brezelfalter <lukas.neumanntuf@gmail.com>
2024-04-18 20:05:22 +0200

    fixed message sending in wakeup.py

--- cogs/wakeup.py
+++ cogs/wakeup.py
@@ -16,14 +16,14 @@ class WakeUp(commands.Cog):
     async def wakeup(self, ctx):
         # send magic packet to my pc, using mac address and ip address
         send_magic_packet("04:42:1a:0d:cf:78", ip_address="192.168.178.146")
-        await ctx.message.send("Sent magic packet to device. MAC: 04:42:1a:0d:cf:78\tIP: 192.168.178.146")
+        await ctx.send("Sent magic packet to device. MAC: 04:42:1a:0d:cf:78\tIP: 192.168.178.146")
 
     
     @commands.command(brief="Wakeup a device on the network specifying the MAC and IP address manually.")
     async def wakeup_any(self, ctx, mac:str, ip:str):
         # send magic packet to any device on the network using the specified mac and ip address
         send_magic_packet(mac, ip_address=ip)
-        await ctx.message.send("Sent magic packet to device. MAC: {mac}\tIP: {ip}")
+        await ctx.send("Sent magic packet to device. MAC: {mac}\tIP: {ip}")
 
 
 
